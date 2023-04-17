2a744b97358642de6533292ac85bf42cedf9899c
maazjnr <goalmaaz@gmail.com>
2023-04-17 11:17:02 +0100

    updates

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var profiles []Profile = []Profile{}
+
+type User struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email"`
+}
+
+type Profile struct {
+	Department  string `json:"Department"`
+	Designation string `json:"Designation"`
+	Employee    User   `json:"Employee"`
+}
+
+func addItem(w http.ResponseWriter, r *http.Request) {
+	var newProfile Profile
+	json.NewDecoder(r.Body).Decode(&newProfile)
+	w.Header().Set("Content-Type", "application/json")
+	profiles = append(profiles, newProfile)
+	json.NewEncoder(w).Encode(profiles)
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	router := mux.NewRouter()
+	router.HandleFunc("/profiles", addItem).Methods("POST")
+	http.ListenAndServe(":5000", router)
 }
