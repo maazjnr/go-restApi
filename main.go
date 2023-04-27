bcd858dc5abec1dd1493ccb8ce840a46b0b25346
maazjnr <goalmaaz@gmail.com>
2023-04-27 14:46:37 +0100

    updates

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -29,8 +30,84 @@ func addItem(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(profiles)
 }
 
+func getAllProfiles(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(profiles)
+}
+
+func getProfiles(w http.ResponseWriter, r *http.Request) {
+	var idParam string = mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idParam)
+
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("ID could not be converted to integer"))
+		return
+	}
+
+	if id >= len(profiles) {
+		w.WriteHeader(400)
+		w.Write([]byte("ID could not be converted to integer"))
+		return
+	}
+
+	profiles := profiles[id]
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(profiles)
+
+}
+
+func updateProfiles(w http.ResponseWriter, r *http.Request) {
+	var idParam string = mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idParam)
+
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("ID could not be converted to integer"))
+		return
+	}
+
+	if id >= len(profiles) {
+		w.WriteHeader(400)
+		w.Write([]byte("ID could not be converted to integer"))
+		return
+	}
+
+	var updateProfile Profile
+	json.NewDecoder(r.Body).Decode(&updateProfile)
+
+	profiles[id] = updateProfile
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(updateProfile)
+}
+
+func deleteProfiles(w http.ResponseWriter, r *http.Request) {
+	var idParam string = mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idParam)
+
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("ID could not be converted to integer"))
+		return
+	}
+
+	if id >= len(profiles) {
+		w.WriteHeader(400)
+		w.Write([]byte("ID could not be converted to integer"))
+		return
+	}
+
+	profiles = append(profiles[:id], profiles[:id+1]...)
+
+	w.WriteHeader(200)
+}
+
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/profiles", addItem).Methods("POST")
+	router.HandleFunc("/profiles/{id}", getProfiles).Methods("Get")
+	router.HandleFunc("/profiles", getAllProfiles).Methods("GET")
+	router.HandleFunc("/profiles/{id}", updateProfiles).Methods("PUT")
+	router.HandleFunc("/profiles/{id}", deleteProfiles).Methods("DELETE")
 	http.ListenAndServe(":5000", router)
 }
